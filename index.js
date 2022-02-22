4f82919ae284f2c7fe37d9f2505a644c67cf1b14
Paul  Vivar <Apvivar92@gmail.com>
2022-02-21 19:09:37 -0800

    Prepped files

---
+++ index.js
@@ -0,0 +1,12 @@
+//tasks
+// Obtain the user's current location.
+// Map the location on a Leaflet map.
+// Allow the user to select a business type from a list and map the five nearest locations on the map using the Foursquare API.
+
+//Functionality
+// What events will your application need?
+// What APIs will you need and in what order?
+// How will you obtain the user's location?
+// How will you add the user's location to the map?
+// How will you get the selected location from the user?
+// How will you add that information to the map?
